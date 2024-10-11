310522c5a4085f2b2fb2340f72fc781826d86434
Leonid Grishenkov <grishenkovleonid@gmail.com>
2024-10-11 10:10:01 +0300

    feat: Add logger with daily log rotation

--- src/utils.py
+++ src/utils.py
@@ -1,35 +1,13 @@
 import logging
 import os
-import sys
+from datetime import date, timedelta
+from logging.handlers import TimedRotatingFileHandler
+from pathlib import Path
 
 import boto3
 import dotenv
 
 dotenv.load_dotenv()
-
-_levels = dict(
-    info=logging.INFO,
-    debug=logging.DEBUG,
-    warn=logging.WARN,
-    warning=logging.WARN,
-    fatal=logging.FATAL,
-    error=logging.ERROR,
-)
-
-try:
-    _level = _levels[os.getenv("APP_LOG_LEVEL", "info")]
-except KeyError:
-    _level = logging.INFO
-
-logging.basicConfig(
-    level=_level,
-    format=r"[%(asctime)s] {%(module)s.%(funcName)s:%(lineno)d} %(levelname)s: %(message)s",
-    datefmt=r"%Y-%m-%d %H:%M:%S",
-    stream=sys.stdout,
-)
-logger = logging.getLogger(name="yandex-cloud-billing-analysis-bot")
-# Desrease `httpx` log level because it's too verbose.
-logging.getLogger("httpx").setLevel(logging.WARNING)
 
 
 def get_s3_instance() -> ...:
@@ -42,3 +20,67 @@ def get_s3_instance() -> ...:
 
 
 AUTHORIZED_USERS = (196255068,)
+
+
+def _create_logger() -> logging.Logger:
+    today: date = date.today()
+    current_week = today - timedelta(days=today.weekday())
+
+    log_dir: Path = Path(__file__).parents[1] / "logs" / current_week.strftime(r"%Y-%m-%d")
+    if not log_dir.exists():
+        log_dir.mkdir(parents=True, exist_ok=True)
+
+    current_date_log_file: Path = log_dir / (today.strftime(r"%Y-%m-%d") + ".log")
+
+    levels: dict = dict(
+        info=logging.INFO,
+        debug=logging.DEBUG,
+        warn=logging.WARN,
+        warning=logging.WARN,
+        fatal=logging.FATAL,
+        error=logging.ERROR,
+    )
+
+    try:
+        level: int = levels[os.getenv("APP_LOG_LEVEL", "info")]
+    except KeyError:
+        level: int = logging.INFO
+
+    logger: logging.Logger = logging.getLogger(name="__this_bot__")
+    logger.setLevel(level)
+
+    handler = TimedRotatingFileHandler(
+        filename=log_dir / current_date_log_file,
+        # when="W0",  # Rotate logs every week on Monday
+        when="midnight",
+        interval=1,  # Interval is set to 1 week
+        backupCount=30,  # Keep 4 backup copies of the log file
+        encoding="utf-8",
+    )
+
+    formatter = logging.Formatter(
+        fmt=r"[%(asctime)s] {%(name)s:%(module)s.%(funcName)s:%(lineno)d} %(levelname)s: %(message)s",
+        datefmt=r"%Y-%m-%d %H:%M:%S",
+    )
+    handler.setFormatter(formatter)
+
+    logger.addHandler(handler)
+
+    # To get all logger instances for project:
+    # loggers = [logging.getLogger(name) for name in logging.root.manager.loggerDict]
+    for extlogger in (
+        logging.getLogger(_name) for _name in ("boto3", "botocore", "httpx", "telegram", "dotenv")
+    ):
+        # Decrease `httpx` package log level bacause it's to verbose
+        # and also writes encrypted bot token.
+        if extlogger.name == "httpx":
+            extlogger.setLevel(logging.WARNING)
+        else:
+            extlogger.setLevel(level)
+
+        extlogger.addHandler(handler)
+
+    return logger
+
+
+logger = _create_logger()
