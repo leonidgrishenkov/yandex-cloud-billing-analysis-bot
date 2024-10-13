796e61408b58aff3837e2a7070804408280b6d8a
Leonid Grishenkov <grishenkovleonid@gmail.com>
2024-10-13 15:17:09 +0300

    add

--- bot/__main__.py
+++ bot/__main__.py
@@ -10,32 +10,46 @@ from telegram.ext import (
 )
 
 from bot import config
-from bot.handlers import error, start, help, message, unknown, callback, weekly, daily
+from bot.handlers import (
+    callback,
+    daily,
+    error,
+    help,
+    message,
+    start,
+    unknown,
+    weekly,
+)
 from bot.utils import logger
+
+COMMAND_HANDLERS = dict(
+    start=start.handle_start_command,
+    help=help.handle_help_command,
+    daily_report=daily.handle_daily_report,
+    weekly_report=weekly.handle_weekly_report,
+)
 
 
 def main() -> ...:
-    logger.info("Starting a bot")
-    # Create the Application.
+    logger.info("Calling application builder")
     app = Application.builder().token(config.TELEGRAM_BOT_TOKEN).build()
 
-    # Commands handlers.
-    app.add_handler(CommandHandler("start", start.handle_start_command))
-    app.add_handler(CommandHandler("help", help.handle_help_command))
-    app.add_handler(CommandHandler("daily_report", daily.handle_daily_report))
-    app.add_handler(CommandHandler("weekly_report", weekly.handle_weekly_report))
+    for command, handler in COMMAND_HANDLERS.items():
+        app.add_handler(
+            CommandHandler(
+                command=command,
+                callback=handler,
+            ),
+        )
 
-    # Messages handlers.
     app.add_handler(MessageHandler(filters.COMMAND, unknown.handle_unknown_command))
     app.add_handler(MessageHandler(filters.TEXT, message.handle_any_message))
 
-    # Erros handlers.
     app.add_error_handler(error.handle_error)
 
-    # Callback query handlers.
     app.add_handler(CallbackQueryHandler(callback.handle_callback_query_buttons))
 
-    logger.info("Start to polling")
+    logger.info("Calling `run_polling`")
     app.run_polling(
         # Time to wait between polling updates from Telegram in seconds.
         poll_interval=1,
