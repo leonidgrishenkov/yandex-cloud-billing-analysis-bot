2d5e12af8daf93b13a9e0388ade256c7790907f5
Leonid Grishenkov <grishenkovleonid@gmail.com>
2024-11-18 11:03:03 +0300

    feat: Add function to dinamically get iam token from Cloud API; Replace IAM token with OAuth token for authentication

--- bot/config.py
+++ bot/config.py
@@ -13,7 +13,7 @@ YC_S3_ENDPOINT_URL: str = "https://storage.yandexcloud.net"
 YC_S3_ADMIN_SA_ACCESS_KEY: str = os.getenv("YC_S3_ADMIN_SA_ACCESS_KEY")
 YC_S3_ADMIN_SA_SECRET_KEY: str = os.getenv("YC_S3_ADMIN_SA_SECRET_KEY")
 
-YC_IAM_TOKEN = os.getenv("YC_IAM_TOKEN")
+YC_OAUTH_TOKEN = os.getenv("YC_OAUTH_TOKEN")
 YC_BILLING_ACC_ID = os.getenv("YC_BILLING_ACC_ID")
 
 APP_LOG_LEVEL: str = os.getenv("APP_LOG_LEVEL", "info")
@@ -27,9 +27,9 @@ if not YC_S3_ADMIN_SA_SECRET_KEY or not YC_S3_ADMIN_SA_ACCESS_KEY:
         "are required to interact with S3 bucket"
     )
 
-if not YC_IAM_TOKEN or not YC_BILLING_ACC_ID:
+if not YC_OAUTH_TOKEN or not YC_BILLING_ACC_ID:
     raise ValueError(
-        "Both `YC_IAM_TOKEN` and `YC_BILLING_ACC_ID` env variables "
+        "Both `YC_OAUTH_TOKEN` and `YC_BILLING_ACC_ID` env variables "
         "are required to interact with Yandex Cloud API"
     )
 
